851a85b348730b58859ddf542836c50a73f8d7a2
Sassan Haradji <sassanh@gmail.com>
2017-08-04 00:23:18 +0430

    Merge utility to be used for nest data structures

---
+++ rest_framework/utils/merge.py
@@ -0,0 +1,14 @@
+###
+Helper function for merging mappings
+###
+import collections
+
+def update(dictionary1, dictionary2):
+    merged = dictionary1.copy()
+    for key, value in dictionary2.items():
+        if isinstance(value, collections.Mapping):
+            updated_value = update(merged.get(key, {}), value)
+            merged[key] = updated_value
+        else:
+            merged[key] = dictionary2[key]
+    return merged
